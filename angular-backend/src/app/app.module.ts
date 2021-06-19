c771ee49d33dc8d5ef9b6a37bca1c9486dd71333
carol80 <carolseb80@gmail.com>
2021-06-19 17:35:34 +0530

    employee, tools added

--- angular-backend/src/app/app.module.ts
+++ angular-backend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './component/employee/employee.component';
+import { EmployeeItemComponent } from './component/employee-item/employee-item.component';
+import { ButtonComponent } from './component/tools/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    EmployeeItemComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
