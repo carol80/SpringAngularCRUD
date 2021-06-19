06d7d930aea1ff86451b00e6c10abf0712067818
carol80 <carolseb80@gmail.com>
2021-06-19 16:50:27 +0530

    add-task component

---
+++ angular-task-tracker/src/app/component/add-task/add-task.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from "../../Tasks"
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+  text: string;
+  day: string;
+  reminder: boolean = false;
+  @Output() addTask: EventEmitter<Task> = new EventEmitter();
+  showAddTask: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe((val) => (this.showAddTask = val));
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmitTask() {
+    if(!this.text || !this.day){
+      alert("Please fill all fields!!")
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    console.log(newTask);
+    this.addTask.emit(newTask);
+
+    this.text = "";
+    this.day = "";
+    this.reminder = false;
+  }
+}
